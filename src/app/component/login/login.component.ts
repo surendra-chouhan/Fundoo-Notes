f141cf1063ef65ebffcfd1a33e7a0860d63b2be2
surendra-chouhan <chouhansurendra028@gmail.com>
2021-05-14 22:37:14 +0530

    Added routing to dashboard on submitting login

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserService } from '../../services/userservice/user.service';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -11,12 +11,13 @@ export class LoginComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private userService: UserService) {
+  constructor(private formBuilder: FormBuilder, private userService: UserService, private router: Router) {
     // create our form group with all the inputs we will be using in the template
     this.form = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
     });
+
   }
 
   submit() {
@@ -25,15 +26,16 @@ export class LoginComponent implements OnInit {
       let arr = [] as any;
 
       let reqObj = {
-                      email: this.form.value.email,
-                      password: this.form.value.password
-                    }
+        email: this.form.value.email,
+        password: this.form.value.password
+      }
       console.log(reqObj);
       this.userService.loginService(reqObj).subscribe((res) => {
         console.log(res);
         arr = res;
         console.log(arr.id);
-        localStorage.setItem('id',arr.id);
+        localStorage.setItem('id', arr.id);
+        this.router.navigate(['/dashboard'])
       }, (error) => {
         console.log(error);
       })
